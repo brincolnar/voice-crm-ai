f08f2cc5d4d482179c7a57c5a25055040e832e90
shogunishere <bc9389@student.uni-lj.si>
2023-10-26 19:20:24 +0200

    simplified backend, removed frontend

--- backend/ai.py
+++ backend/ai.py
@@ -8,14 +8,12 @@ load_dotenv(find_dotenv())
 ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def init():
-    openai.api_key = OPENAI_API_KEY
+openai.api_key = OPENAI_API_KEY
 
-def whisper(mp3_path):
-    with open(mp3_path, "rb") as audio_file:
+def whisper(path):
+    with open(path, "rb") as audio_file:
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
     
     text = transcript['text']
-    print('text: ', text)
 
     return text
--- backend/app.py
+++ backend/app.py
@@ -8,88 +8,49 @@ from werkzeug.utils import secure_filename
 from shutil import move
 from datetime import datetime
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config['result_backend'],
-        broker=app.config['broker_url']
-    )
-    celery.conf.update(app.config)
-    celery.conf.update(CELERY_IMPORTS=('your_script_name',))
-    return celery
 
 app = Flask(__name__)
-app.config.update(
-    broker_url='redis://localhost:6379/0',
-    result_backend='redis://localhost:6379/0'
-)
 
-celery = make_celery(app)
 CORS(app, supports_credentials=True, origins=['*'])
-    
-@celery.task
-def convert_task(file_path):
-    # Convert OGA to WAV using ffmpeg-python
-    wav_path = file_path.replace('.oga', '.wav')
-    try:
-        ffmpeg.input(file_path).output(wav_path).run(overwrite_output=True)
-    except ffmpeg.Error as e:
-        print(f'ffmpeg error: {e.stderr.decode()}')
-        return 'Conversion failed'
-
-    # Convert WAV to MP3
-    mp3_path = file_path.replace('.oga', '.mp3')
-    try:
-        sound = AudioSegment.from_wav(wav_path)
-        sound.export(mp3_path, format="mp3")
-    except Exception as e:
-        print(f"Error converting to MP3: {e}")
-        return 'Error converting to MP3'
-
-    # Call transcribe task
-    transcribe_task.delay(mp3_path)
-
-@celery.task
-def transcribe_task(mp3_path):
-    # Initialize your whisper module (replace `init` and `whisper` with your actual initialization and transcription methods)
-    try:
-        init()
-        text = whisper(mp3_path)
-    except Exception as e:
-        print(f"Error during transcription: {e}")
-        return 'Error during transcription'
-
-    return f'File transcribed successfully: {text}'
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
-    if 'audio' not in request.files:
+    if 'audio_data' not in request.files:
         return jsonify({'error': 'No file uploaded'}), 400
 
-    file = request.files['audio']
-    filename = secure_filename(file.filename)
+    file_ = request.files['audio_data']
+    print("Raw file: ")
+    print(file_)
 
+    filename = file_.filename.replace(':', '_') + '.wav'
+    print('Audio filename 1: ', filename)
+    
     # Get the current timestamp and append it to the filename
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"{timestamp}_{filename}"
 
     # Save the file to the uploads directory
-    uploads_dir = 'uploads'
+    uploads_dir = '.\\uploads'
     os.makedirs(uploads_dir, exist_ok=True)
 
     file_path = os.path.join(uploads_dir, filename)
-    print("file_path: ", file_path)
+    print('Audio filename 2: ', file_path)
     try:
-        file.save(file_path)
-        print(f"File saved with timestamp: {filename}")
+        file_.save(file_path)
     except Exception as e:
         print(f"Error saving file: {e}")
         return jsonify({'error': 'Error saving file'}), 500
     
-    # Initiate the convert task
-    convert_task.delay(file_path)
+    # Initialize your whisper module (replace `init` and `whisper` with your actual initialization and transcription methods)
+    try:
+        text = whisper(file_path)
+    except Exception as e:
+        print(f"Error during transcription: {e}")
+        return 'Error during transcription'
 
-    return jsonify({'message': 'File upload successful'}), 200
+    print("Transcription: ", text)
+
+    return jsonify({'message': text}), 200
 
 @app.after_request
 def after_request(response):
